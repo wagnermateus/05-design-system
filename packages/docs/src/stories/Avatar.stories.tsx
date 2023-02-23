a034943beedba4d3a29305c97887e020015842ff
wagnermateus <wagnermateus7@gmail.com>
2023-02-23 20:04:33 +0100

    Melhorias no Storybook - Controles nos componentes

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/wagnermateus.png',
     alt: 'Wagner Mateus',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
